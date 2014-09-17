89c6ff12ba837870ba3f264bc7bcb5f204e1f3c5
Lomscroft <Lomscroft@172.29.86.57>
2014-09-17 14:44:17 -0400

    test commit

---
+++ testFormationContinue/src/testformationcontinue/TestFormationContinue.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testformationcontinue;
+
+/**
+ *
+ * @author Lomscroft
+ */
+public class TestFormationContinue {
+
+    public void testAfficher() {
+        System.out.println("Test");
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
